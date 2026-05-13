dbdf8058a1b366c379640ac7aff847236005f4e3
krishnakumar <krishacker125@gmail.com>
2026-05-13 19:20:48 +0530

    Merge conflict

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://notenestapp-7hhn.onrender.com/api",
+  "https://note-nestapp.vercel.app",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
