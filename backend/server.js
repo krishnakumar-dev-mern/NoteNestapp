20bbb3831c71f34fc490ecfe7e0d084507ed25a7
krishnakumar <krishacker125@gmail.com>
2026-05-13 18:52:12 +0530

    Update CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://notenestapp-7hhn.onrender.com/api",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
